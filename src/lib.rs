fe24cc8944d93d537f1990e7ca6d20093e7df37f
Daniel Mason <daniel@danielmason.com>
2022-07-09 23:33:04 +0100

    Add some basic documentation and tests

--- src/lib.rs
+++ src/lib.rs
@@ -8,10 +8,24 @@ fn choose_from_pool(mut pool: Vec<u8>, count: usize) -> Vec<u8> {
     chosen
 }
 
+/// Choose 5 numbers from a pool of numbers
+/// ```rust
+/// use lottery_picker::choose_numbers;
+///
+/// let pool: Vec<_> = (1..100).collect();
+/// assert_eq!(choose_numbers(pool).len(), 5);
+/// ```
 pub fn choose_numbers(pool: Vec<u8>) -> Vec<u8> {
     choose_from_pool(pool, 5)
 }
 
+/// Choose 2 numbers from a pool of numbers
+/// ```rust
+/// use lottery_picker::choose_lucky_stars;
+///
+/// let pool: Vec<_> = (1..100).collect();
+/// assert_eq!(choose_lucky_stars(pool).len(), 2);
+/// ```
 pub fn choose_lucky_stars(pool: Vec<u8>) -> Vec<u8> {
     choose_from_pool(pool, 2)
 }
